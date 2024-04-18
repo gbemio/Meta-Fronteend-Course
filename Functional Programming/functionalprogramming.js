aacf788dfdca0cd9864a5a3bc1c5b8821f6eab52
Odukoya Gbemisola <folaryz@gmail.com>
2024-04-18 19:02:47 +0100

    Passed arguments for styleAndCelebrate function

--- Functional Programming/functionalprogramming.js
+++ Functional Programming/functionalprogramming.js
@@ -2,6 +2,7 @@
 function consoleStyler(color, background, fontSize, txt) {
     var message = "%c" + txt;
     var style = `color: ${color};`
+    style += `background: ${background};`
     style += `font-size: ${fontSize};`
     console.log(message, style);
 }
@@ -9,7 +10,7 @@ consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!' )
 
 // Task 2: Build another console log message generator
 function celebrateStyler(reason) {
-    var fontStyle = "color: tomato; font-size: 50px";
+    var fontStyle = "color: tomato; font-size: 50px;"
     if (reason == "birthday") {
     console.log(`%cHappy birthday`, fontStyle);
     } else if (reason == "champions") {
@@ -24,8 +25,8 @@ celebrateStyler('birthday')
 
 
 // Task 4: Insert a congratulatory and custom message
-function styleAndCelebrate() {
-    consoleStyler(color, background, fontSize, txt); 
+function styleAndCelebrate(color, background, fontSize, txt, reason) {
+    consoleStyler(color, background, fontSize, txt);
     celebrateStyler(reason);
 }
 styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', 'champions')
