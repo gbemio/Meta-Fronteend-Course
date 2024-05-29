5e38dec4b4fd8ef11d85cd230151d309ecb2b854
Odukoya Gbemisola <folaryz@gmail.com>
2024-05-29 14:48:01 +0100

    Using Browser Router

---
+++ react-router-navbar/src/App.js
@@ -0,0 +1,21 @@
+import AboutMe from './AboutMe';
+import Homepage from './Homepage';
+import {Routes, Route, Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='nav'>
+        <Link to='/' className='nav-item'>Homepage</Link>
+        <Link to='/about-me' className='nav-item'>About Me</Link>
+       </nav>
+        <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/about-me' element={<AboutMe/>}/>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
