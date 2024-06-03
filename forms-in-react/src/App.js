bef02c2f124668281c656fa15970ebf6fae7c349
Odukoya Gbemisola <folaryz@gmail.com>
2024-06-03 14:52:50 +0100

    Creating a Feedback form with controlled component

--- forms-in-react/src/App.js
+++ forms-in-react/src/App.js
@@ -1,32 +1,84 @@
 import {useState} from 'react';
 import './App.css';
 
+// Feedback form with controlled component that ahs a range
+
 function App() {
-  const [name, setName] = useState("");
+  const [score, setScore] = useState('10');
+const [comment, setComment] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setName("");
-    console.log("Form Submitted")
+    if (Number(score) <= 5 && comment.length <= 10) {
+      alert("Please provide a comjment expllaining why the experience was poor.")
+      return;
+    }
+
+    console.log("Form submitted");
+    setComment("");
+    setScore("10");
   }
 
   return (
     <div className='App'>
       <form onSubmit={handleSubmit}>
         <fieldset>
+          <h2>Feedback form</h2>
           <div className='Field'>
-            <label htmlFor='name'>Name:</label>
-            <input id='name' type='text' placeholder='Name' name='name' value={name} onChange={e => setName(e.target.value)}/>
+            <label>Score:</label>
+            <input
+              type='range'
+              min="0"
+              max="10"
+              value={score}
+              onChange={e => setScore(e.target.value)}
+               />
+            <div className='Field'>
+              <label>Comment:</label>
+              <textarea value={comment} onChange={e => setComment(e.target.value)}/>
+            </div>
           </div>
-          <button disabled={!name} type='submit'>Submit</button>
+          <button type='submit'>Submit</button>
         </fieldset>
       </form>
-    </div>
+      </div>
   )
 }
 
-export default App;
 
+
+
+
+
+
+// Controlled forn
+// function App() {
+//   const [name, setName] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setName("");
+//     console.log("Form Submitted")
+//   }
+
+//   return (
+//     <div className='App'>
+//       <form onSubmit={handleSubmit}>
+//         <fieldset>
+//           <div className='Field'>
+//             <label htmlFor='name'>Name:</label>
+//             <input id='name' type='text' placeholder='Name' name='name' value={name} onChange={e => setName(e.target.value)}/>
+//           </div>
+//           <button disabled={!name} type='submit'>Submit</button>
+//         </fieldset>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// Uncontrolled form
 // function App() {
 
 //   return (
