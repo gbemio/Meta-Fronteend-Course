7d781ad04dc4e8911b2e9ad4fe817e8891db5b4e
Odukoya Gbemisola <folaryz@gmail.com>
2024-06-03 14:10:21 +0100

    Created Controlled Input Form in React

---
+++ forms-in-react/src/App.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <form>
+        <fieldset>
+          <div className='Field'>
+            <label>Name:</label>
+            <input type='text' placeholder='Name' name='name'/>
+          </div>
+          <button type='submit'>Submit</button>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
+
+export default App;
