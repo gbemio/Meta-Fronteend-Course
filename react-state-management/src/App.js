fe55fb0a681cb252d8eb18ab796cedad4c090b99
Odukoya Gbemisola <folaryz@gmail.com>
2024-06-05 14:27:08 +0100

    Working with complex Data in useState

--- react-state-management/src/App.js
+++ react-state-management/src/App.js
@@ -41,5 +41,78 @@ export default App;
 //         </button>
 //       </div>
 //     </div>
+
 //   )
 // }
+
+
+// example of holding state in an object and updation it based on user-generated events
+
+// import { useState } from "react"; 
+ 
+// export default function App() { 
+//   const [greeting, setGreeting] = useState({ greet: "Hello, World" }); 
+//   console.log(greeting, setGreeting); 
+ 
+//   function updateGreeting() { 
+//     setGreeting({ greet: "Hello, World-Wide Web" }); 
+//   } 
+ 
+//   return ( 
+//     <div> 
+//       <h1>{greeting.greet}</h1> 
+//       <button onClick={updateGreeting}>Update greeting</button> 
+//     </div> 
+//   ); 
+// } 
+
+
+// Correct way to update the state object in React when using useState
+
+// import { useState } from "react"; 
+ 
+// export default function App() { 
+//   const [greeting, setGreeting] = useState({ greet: "Hello, World" }); 
+//   console.log(greeting, setGreeting); 
+ 
+//   function updateGreeting() { 
+//     const newGreeting = {...greeting}; 
+//     newGreeting.greet = "Hello, World-Wide Web"; 
+//     setGreeting(newGreeting); 
+//   } 
+ 
+//   return ( 
+//     <div> 
+//       <h1>{greeting.greet}</h1> 
+//       <button onClick={updateGreeting}>Update greeting</button> 
+//     </div> 
+//   ); 
+// } 
+
+
+// Updating the state object using arrow functions
+
+// import { useState } from "react"; 
+ 
+// export default function App() { 
+//   const [greeting, setGreeting] = useState( 
+//     { 
+//         greet: "Hello", 
+//         place: "World" 
+//     } 
+//   ); 
+//   console.log(greeting, setGreeting); 
+ 
+//   function updateGreeting() { 
+//     setGreeting(prevState => { 
+//         return {...prevState, place: "World-Wide Web"} 
+//     }); 
+//   } 
+ 
+//   return ( 
+//     <div> 
+//       <h1>{greeting.greet}, {greeting.place}</h1> 
+//       <button onClick={updateGreeting}>Update greeting</button> 
+//     </div> 
+//   ); 
+// } 
