7da2404d49e43f49c941f9b5c39ca0a8dd585938
Odukoya Gbemisola <folaryz@gmail.com>
2024-06-10 21:37:41 +0100

    Children Manipulation Using Apis

---
+++ children-manipulation/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+const Row = ({children, spacing}) => {
+  const childStyle = {
+    marginLeft: `${spacing}px`,
+  };
+
+  return (
+    <div classname="Row">
+      {React.Children.map(children, (child,index) => {
+        return React.cloneElement(child, {
+          style: {
+            ...child.props.style,
+            ...(index > 0 ? childStyle : {}),
+          },
+        });
+      })}
+    </div>
+  );
+};
+
+function LiveOrders() {
+  return (
+    <div className='App'>
+      <Row spacing={32}>
+        <p>Pizza Mrgaritta</p>
+        <p>2</p>
+        <p>30$</p>
+        <p>18:30</p>
+        <p>John</p>
+      </Row>
+    </div>
+  );
+};
+
+export default LiveOrders;
